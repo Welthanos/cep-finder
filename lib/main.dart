b397ce8f91f8e321f430cd27f186421c381cf628
Welthanos <welthanos_dev@outlook.com>
2022-05-29 18:09:31 -0300

    style: appBar

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CEP Finder',
+      home: MyHomePage(
+        title: SizedBox(
+          height: 60,
+          child: Image.asset('assets/images/logo.png'),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final Widget title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        toolbarHeight: 75,
+        title: widget.title,
+      ),
+      body: Column(),
+    );
+  }
+}
